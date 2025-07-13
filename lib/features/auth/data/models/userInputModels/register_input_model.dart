4a926eda4fff5b4c71c8875a62ae8b7e5c3bdb89
Eng-Moataz <mizo201515@gmail.com>
2025-07-13 22:49:14 +0300

    create user input models

---
+++ lib/features/auth/data/models/userInputModels/register_input_model.dart
@@ -0,0 +1,31 @@
+import 'package:json_annotation/json_annotation.dart';
+
+@JsonSerializable()
+class RegisterInputModel {
+  final String userName;
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String password;
+  final String rePassword;
+  final String phoneNumber;
+  RegisterInputModel({
+    required this.userName,
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.password,
+    required this.rePassword,
+    required this.phoneNumber,
+  });
+
+  Map<String, dynamic> toJson() => {
+    'username': userName,
+    'firstName': firstName,
+    'lastName': lastName,
+    'email': email,
+    'phone': phoneNumber,
+    'password': password,
+    'rePassword': rePassword,
+  };
+}
